897baf2659ca394e747f164b62b760f189e84e03
Harum Helmy <harumendhah@gmail.com>
2019-08-05 17:26:14 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let kcWin = record.find(function(elem) {
+    return elem.result === "W"});
+    if (kcWin)
+      return kcWin.year 
+    else 
+      return undefined
+  }
